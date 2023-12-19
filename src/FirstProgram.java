48ad6bd8967fe8b3a279a3783866b9377c913cc0
saqlaintandel <saqlaintandel96@gmail.com>
2023-12-19 23:03:13 +0530

    Code Updated

--- src/FirstProgram.java
+++ src/FirstProgram.java
@@ -3,6 +3,7 @@ package src;
 public class FirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World");
+        System.out.println("This is my First Java Program");
     }
 }
